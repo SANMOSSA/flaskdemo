771ab4767fc61a509224ad3cb880c8f2229768e6
SANMOSSA <105128160+SANMOSSA@users.noreply.github.com>
2023-07-17 20:31:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-   print('Request for index page received')
-   return {'message': 'Hello, World!'}
-
-
+   return {'Dev': 'Dev'}
 
 if __name__ == '__main__':
    app.run()
+
